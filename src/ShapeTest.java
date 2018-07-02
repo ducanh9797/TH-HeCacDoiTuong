9d7abd2e0fa27549c5dcd10b44ae7a2208a1defd
KING JAME <ducanhdovts@gmail.com>
2018-07-02 14:52:41 +0700

    ThucHanh he cac doi tuong hinh hoc

---
+++ src/ShapeTest.java
@@ -0,0 +1,7 @@
+public class ShapeTest {
+    public static void main(String[] args) {
+        ClassShape shapetest = new ClassShape();
+        System.out.println(shapetest.toString());
+
+    }
+}
